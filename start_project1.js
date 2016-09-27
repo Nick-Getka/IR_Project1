8031083fa566444afc1a55110313de63190aadb4
Nick Getka <ngetka@ngetka.localdomain>
2016-09-26 22:48:41 -0400

    finished validate_argv

--- start_project1.js
+++ start_project1.js
@@ -1,4 +1,14 @@
-var val = require('./validate_argv.js');
-if(val.validate){
+//Nicholas Getka
+//IR Project 1
+
+//This is the init file for the project. This file takes in the data file
+//and funnels the data through the preprocessors and the index builders
+//as well as create temp files that will be used to create the final
+// index files
+var val = require('./validate_argv.js').validate();
+if(val){
   console.log("SUCCESS AGAIN!");
+}
+else {
+  
 }
--- validate_argv.js
+++ validate_argv.js
@@ -1,27 +1,26 @@
-module.exports = function() {
-  this.validate = function() {
-   var isTrue = false;
-   if(process.argv[2] == null){
-     console.log("The path to the data file and the memory constraint must be"+
-     " passed as commandline arguements");
-   }
-   else {
-     if(process.argv[2] == 'help'){
-       console.log("command line arguements: 1 - /path/to/data.file, 2 - memory constraint (1000, 10000, 100000)");
-     }
-     else{
-       if(process.argv[4] == null){
-         console.log("A command is missing");
-       }
-       else if(process.argv[4] != 1000 || process.argv[4] != 10000 || process.argv[4] != 100000){
-         console.log("The memory constraint must be 1000, 10000, or 100000");
-       }
-       else{
-         console.log("Success!");
-         isTrue = true;
-       }
-     }
-   }
-   return isTrue;
- }
+//Nicholas Getka
+//IR Project 1
+
+//This code could have been included in the start_project1.js file
+//but it was serperated mostly so I could learn how to export functions between files
+//in the future the other js files will more substantially add to the project
+//including many more functions and data structures
+function validate() {
+  var isTrue = false;
+  if(process.argv[2] == null || process.argv[3] == null){
+    console.log("The path to the data file and the memory constraint must be passed as commandline arguements");
+    console.log("command line arguements: 1 - /path/to/data.file, 2 - memory constraint (1000, 10000, 100000)");
+  }
+  else {
+    var mem = parseInt(process.argv[3]);
+    if(mem != 1000 && mem != 10000 && mem != 100000){
+      console.log("The memory constraint must be 1000, 10000, or 100000");
+    }
+    else{
+      console.log("Success!");
+      isTrue = true;
+    }
+  }
+  return isTrue;
 }
+module.exports.validate = validate;
